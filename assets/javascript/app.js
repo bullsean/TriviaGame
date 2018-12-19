9dd29cf69db9d9b19bce772b00e4b081713e9190
bullsean <41093836+bullsean@users.noreply.github.com>
2018-12-18 21:36:36 -0600

    Stopwatch is working, hiding and showing is working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,72 @@
 //Create button to start game
-$("#startButton").on("click", function () {
-    console.log("Start button clicked!")
-})
+window.onload = function () {
+    $("#startButton").click(stopwatch.start);
+    $("#timer").hide();
+    $("#questionsDiv").hide();
+    $("#endMessage").hide();
+};
 //Create initial variables (time, clockIsRunning boolean, gameStarted boolean)
+
+var intervalId;
+var clockIsRunning = false;
+var gameStarted = false;
+var stopwatch = {
+    time: 10,
+    start: function() {
+        console.log("Start button clicked!");
+        intervalId = setInterval(stopwatch.count, 1*1000);
+        clockIsRunning = true;
+        gameStarted = true;
+        $("#startButton").hide();
+        $("#timer").show();
+
+        if(gameStarted === true) {
+            $("#questionsDiv").show();
+        }
+
+    },
+    count: function() {
+        stopwatch.time--;
+        var currentTime = stopwatch.timeConverter(stopwatch.time);
+        $("#timer").text(currentTime);
+        console.log(stopwatch.time)
+
+        if(stopwatch.time === 0) {
+            $("#questionsDiv").hide();
+            $("#timer").hide();
+            $("#endMessage").show();
+            stopwatch.stop();
+        }
+    },
+
+    stop: function() {
+        clearInterval(intervalId);
+        clockIsRunning = false;
+        console.log("Timer has stopped!")
+    },
+
+    timeConverter: function(time) {
+
+        //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+        var minutes = Math.floor(time / 60);
+        var seconds = time - (minutes * 60);
+    
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+    
+        if (minutes === 0) {
+          minutes = "00";
+        }
+    
+        else if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+    
+        return minutes + ":" + seconds;
+      }
+
+}
 //On start button click, all questions are unhidden, start button is hidden
 //When start button is hit, countdown timer starts
 //When countdown timer stops, all questions hide, score is calculated
