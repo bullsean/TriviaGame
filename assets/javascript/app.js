91185766e8bb021a028cd5bd8b99aabc6340bd3a
bullsean <41093836+bullsean@users.noreply.github.com>
2018-12-18 20:34:49 -0600

    Start button initiated

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//Create button to start game
+$("#startButton").on("click", function () {
+    console.log("Start button clicked!")
+})
+//Create initial variables (time, clockIsRunning boolean, gameStarted boolean)
+//On start button click, all questions are unhidden, start button is hidden
+//When start button is hit, countdown timer starts
+//When countdown timer stops, all questions hide, score is calculated
+//Create questions using radio from Bootstrap
+//Create logic to tell if answer selected is correct or incorrect
+//Use an object to display questions and answer options(array)
+//Create javascript to answer the questions
