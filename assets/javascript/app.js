8a3a943fc78a7114c09123a80013f4c7114d2426
bullsean <41093836+bullsean@users.noreply.github.com>
2018-12-20 22:25:13 -0600

    Not getting much further...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,46 @@ window.onload = function () {
 };
 //Create initial variables (time, clockIsRunning boolean, gameStarted boolean)
 
+var question = [
+    {prompt: "How do you say 'hello' in Italiano?",
+    option1: "heyo",
+    option2: "salud",
+    option3: "ciao",
+    answer: "ciao"
+    },
+    {prompt: "What colors are included in the Italian flag?",
+    option1: "green, blue, brown",
+    option2: "red, yellow, blue",
+    option3: "green, white, red",
+    answer: "green, white, red"
+    },
+    {prompt: "What is the fashion capital of Italy?",
+    option1: "Rome",
+    option2: "Venice",
+    option3: "Milan",
+    answer: "Milan"
+    },
+    {prompt: "What major Italian car brand recently made its way to America?",
+    option1: "Subaru",
+    option2: "Fiat",
+    option3: "Clio",
+    answer: "Fiat"
+    },
+    {prompt: "What's the best Italian baby girl name?",
+    option1: "Giorgia",
+    option2: "Cinzia",
+    option3: "Noemi",
+    answer: "Giorgia"
+    }
+]
 var intervalId;
 var clockIsRunning = false;
 var gameStarted = false;
+var correctCount = 0;
+var incorrectCount = 0;
+var unansweredCount = 0;
 var stopwatch = {
-    time: 10,
+    time: 30,
     start: function() {
         console.log("Start button clicked!");
         intervalId = setInterval(stopwatch.count, 1*1000);
@@ -19,12 +54,32 @@ var stopwatch = {
         gameStarted = true;
         $("#startButton").hide();
         $("#timer").show();
+        $("#questionsDiv").show();
 
-        if(gameStarted === true) {
-            $("#questionsDiv").show();
+        for (var i = 0; i<question.length; i++) {
+            var questionAnswered = false;
+            $("#questionText").text(question[i].prompt);
+            $("#choice1").text(question[i].option1);
+            $("#choice2").text(question[i].option2);
+            $("#choice3").text(question[i].option3);
+            stopwatch.radioCheck();
+            //call function that checks whether radio button checked
+            
+            
         }
 
     },
+
+    radioCheck: function() {
+        console.log("Radio button being checked");
+        for (var i=0;i<document.questionSpace.multipleChoice.length;i++){
+            if (document.questionSpace.multipleChoice[i].checked==true){
+            correctCount++;
+            break //exist for loop, as target acquired.
+            }
+            }
+    },
+
     count: function() {
         stopwatch.time--;
         var currentTime = stopwatch.timeConverter(stopwatch.time);
